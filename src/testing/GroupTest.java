0d134f847670efd368aa195fe0e0c10eb0df920f
wouterrvandamme <wouter@localhost.localdomain>
2014-12-13 22:52:20 +0100

    testing

--- src/testing/GroupTest.java
+++ src/testing/GroupTest.java
@@ -9,42 +9,22 @@ import org.junit.Test;
 
 import domain.Group;
 import domain.Question;
+import domain.QuestionText;
 import domain.User;
 
 public class GroupTest {
 
 	
-	private Group group;
-	
-	private String name;
-	
-	private User admin;
-		
-	private ArrayList<User> users;
-		
-	private ArrayList<Question> questions;
-	
-	private boolean userAddQuestion, userInviteFriends;
-	
-	private int id;
 	
 	
 	@Before
 	public void setup(){
-		group = new Group();
-		name = "testGroup";
-		admin = new User("adminTest","adminTest","adminTest");
-		users = new ArrayList<>();
-		users.add(new User("test","test","test"));
 		
-		questions = new ArrayList<>();
-		userAddQuestion = true;
-		userInviteFriends = true;
-		id = 1;
 	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void adddUser() {
+		
 	}
 	
 }
